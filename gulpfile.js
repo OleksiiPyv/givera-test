4118c87b15773a2a88495cf29c35c7ebb753acfb
Oleksii Pyvovarov <pivolesha@gmail.com>
2022-06-17 18:22:29 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+
+gulp.task("styles", function () {
+    return gulp
+        .src("./src/scss/main.scss")
+        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+        .pipe(
+            autoprefixer({
+                cascade: false,
+            })
+        )
+        .pipe(
+            cleanCSS({
+                level: 2,
+            })
+        )
+        .pipe(gulp.dest("./build/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("watch", function () {
+    browserSync.init({
+        server: {
+            port: 3000,
+            baseDir: "./",
+        },
+    });
+    gulp.watch("./src/scss**/*.scss", gulp.series('styles'));
+    gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+gulp.task('build', gulp.series('styles'))
+
+gulp.task('dev', gulp.series('build', 'watch'))
